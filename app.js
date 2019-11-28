7c9866358fd428d5eef304a620507ea39ddafd96
easy-rascrutka <51583008+easy-rascrutka@users.noreply.github.com>
2019-11-28 07:28:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ particlesJS('particles-js',
         }
       },
       "size": {
-        "value": 5,
+        "value": 50,
         "random": true,
         "anim": {
           "enable": false,
@@ -61,7 +61,7 @@ particlesJS('particles-js',
         }
       },
       "line_linked": {
-        "enable": true,
+        "enable": false,
         "distance": 150,
         "color": "#ffffff",
         "opacity": 0.4,
